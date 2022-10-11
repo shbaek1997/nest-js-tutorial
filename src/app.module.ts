fc7e298e9325a5bffc6a733d65d251ce2f3e68ad
shbaek1997 <shbaek1997@gmail.com>
2022-10-11 17:56:26 +0900

    Refac: movie module, app module 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app/app.controller';
 
+//AppModule은 main이 합치는 모듈 (메인용), 나머지는 imports에 정리하는 것이 좋음.
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
